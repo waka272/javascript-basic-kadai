c002dfeab1abf6d41cd1bc4b0a2648524fc07cbd
waka272 <w.kanno3333@gmail.com>
2024-09-13 19:34:38 +0700

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+// 関数を作成する
+const honjitsunohiduke = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth()+1;
+    const day = today.getDate();
+    console.log(year + '年'+ month+ '月'+ day+ '日');
+
+}
+
+// 関数を呼び出す
+honjitsunohiduke();
